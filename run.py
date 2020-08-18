9d82f8df3ba178c07017b6a35af91ac044568411
罗亚东 <luoyadong@bilibili.com>
2020-08-18 11:38:59 +0800

    输入模型图像size归一化, 删除无用的依赖

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 # /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, current_app
+from flask import Flask
 from app.controller import player_app
-from app.factory import LogManager
 
 app = Flask(__name__)
 app.register_blueprint(player_app, url_prefix='/player')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2233, debug=True)
+    app.run(host="0.0.0.0", port=2233, debug=True, threaded=True)
