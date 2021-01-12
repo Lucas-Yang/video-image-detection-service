decd932ced253394493df18e44feab3bfa59ce2e
罗亚东 <luoyadong@bilibili.com>
2021-01-12 16:14:11 +0800

    新增单元测试

---
+++ tests/func_test.py
@@ -0,0 +1,18 @@
+"""
+重要类函数单元测试
+"""
+from app.model import ImageIndex
+from app.model import PlayerIndex
+
+
+class TestFunc(object):
+    """ 函数接口测试类
+    """
+    def black_white_frame_detection_test(self):
+        pass
+
+    def blurred_frame_detection_test(self):
+        pass
+
+    def frame_ocr_test(self):
+        pass
