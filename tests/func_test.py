684d16f27c2ce324fa7b51560ee11ce731d41939
zzy <zhangzheyuan@bilibili.com>
2021-03-09 18:22:23 +0800

    2021.3.9 ZzY 1. 添加重要类函数单元测试，添加黑白屏检测，花屏检测，OCR测试 2. 添加测试用数据1张扭曲，1张花屏，1张正常，1张白屏，1张OCR

--- tests/func_test.py
+++ tests/func_test.py
@@ -1,6 +1,9 @@
 """
 重要类函数单元测试
 """
+
+import cv2
+import os
 from app.model import ImageIndex
 from app.model import PlayerIndex
 
@@ -8,11 +11,52 @@ from app.model import PlayerIndex
 class TestFunc(object):
     """ 函数接口测试类
     """
-    def black_white_frame_detection_test(self):
-        pass
+    module_path = os.path.dirname(__file__)
 
-    def blurred_frame_detection_test(self):
-        pass
+    # 函数必须以test_打头
+    def test_black_white_frame_detection_true(self):
+        filename = self.module_path + '/image_data/white.jpg'
+        img = cv2.imread(filename)
+        img_bytes = cv2.imencode('.jpg', img)[1]
+        a = ImageIndex(img_bytes)
+        assert a.black_white_frame_detection() is True
 
-    def frame_ocr_test(self):
+    def test_black_white_frame_detection_false(self):
+        filename = self.module_path + '/image_data/normal.jpg'
+        img = cv2.imread(filename)
+        img_bytes = cv2.imencode('.jpg', img)[1]
+        a = ImageIndex(img_bytes)
+        assert a.black_white_frame_detection() is False
+
+    def test_blurred_frame_detection_false(self):
+        filename = self.module_path + '/image_data/white.jpg'
+        img = cv2.imread(filename)
+        img_bytes = cv2.imencode('.jpg', img)[1]
+        a = ImageIndex(img_bytes)
+        assert a.blurred_frame_detection() is False
+
+    def test_blurred_frame_detection_zero(self):
+        filename = self.module_path + '/image_data/normal.jpg'
+        img = cv2.imread(filename)
+        img_bytes = cv2.imencode('.jpg', img)[1]
+        a = ImageIndex(img_bytes)
+        assert a.blurred_frame_detection() is True
+
+    # -1 是 timeout
+    def test_blurred_frame_detection_minus(self):
+        filename = self.module_path + '/image_data/cate1.jpg'
+        img = cv2.imread(filename)
+        img_bytes = cv2.imencode('.jpg', img)[1]
+        a = ImageIndex(img_bytes)
+        assert a.blurred_frame_detection() == -1
+
+    def test_frame_ocr(self):
+        filename = self.module_path + '/image_data/ocr.jpg'
+        img = cv2.imread(filename)
+        img_bytes = cv2.imencode('.jpg', img)[1]
+        a = ImageIndex(img_bytes)
+        assert a.frame_ocr() is not None
+
+    # 暂时还无接口
+    def error_frame_detection_test(self):
         pass
