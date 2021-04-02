e49d9fc95ac77c7dc29e9f66893fd04757ab512f
罗亚东 <luoyadong@bilibili.com>
2021-04-02 14:54:29 +0800

    异步任务调整，只对启动耗时以及首帧耗时做固定帧率

--- app/tasks.py
+++ app/tasks.py
@@ -7,15 +7,10 @@ from celery import Celery
 from ffmpy import FFmpeg
 from app.factory import LogManager
 from app.model import PlayerIndex
+from app.data import VideoQualityItem
 import traceback
 
 logger = LogManager('server.log').logger
-
-# plz double check redis database num, nvm-t-ep-common-01
-# brokers = 'redis://172.22.119.29:6379/5'
-# backend = 'redis://172.22.119.29:6379/6'
-brokers = 'redis://0.0.0.0:6379/3'
-backend = 'redis://0.0.0.0:6379/4'
 
 celery_app = Celery('player-tasks')
 celery_app.config_from_object('config')
@@ -35,22 +30,40 @@ def cv_index_task(cv_info_dict):
     task_success_flag = True
     try:
         file_path = cv_info_dict.get("temp_video_path")
-        cfr_video_path = cv_info_dict.get("temp_video_path").split('.mp')[0] + "1.mp4"
-        logger.info(cfr_video_path)
-        ff = FFmpeg(
-            inputs={file_path: None},
-            outputs={
-                cfr_video_path: '-y -vf fps=32 -vsync cfr'
-            }
-        )
-        ff.run()
-        cv_info_dict["temp_video_path"] = cfr_video_path
-        model_handler = PlayerIndex(cv_info_dict=cv_info_dict)
-        cv_result_index = model_handler.get_cv_index()
-        cv_result_index = json.loads(json.dumps(cv_result_index))
-        os.remove(cv_info_dict.get("temp_video_path"))  # 删除临时视频文件
-        os.remove(file_path)  # 删除临时固定帧率源视频
-        logger.info(cv_result_index)
+        if (
+                VideoQualityItem.STARTAPPDOUYIN.name in cv_info_dict.get("index_types")
+                or VideoQualityItem.STARTAPP.name in cv_info_dict.get("index_types")
+                or VideoQualityItem.STARTAPPCOMIC.name in cv_info_dict.get("index_types")
+                or VideoQualityItem.STARTAPPIXIGUA.name in cv_info_dict.get("index_types")
+                or VideoQualityItem.STARTAPPTENCENT.name in cv_info_dict.get("index_types")
+                or VideoQualityItem.STARTAPPYOUKU.name in cv_info_dict.get("index_types")
+                or VideoQualityItem.STARTAPPIQIYI.name in cv_info_dict.get("index_types")
+        ):
+            cfr_video_path = cv_info_dict.get("temp_video_path").split('.mp')[0] + "1.mp4"
+            logger.info(cfr_video_path)
+            ff = FFmpeg(
+                inputs={file_path: None},
+                outputs={
+                    cfr_video_path: '-y -vf fps=32 -vsync cfr'
+                }
+            )
+            ff.run()
+            cv_info_dict["temp_video_path"] = cfr_video_path
+            model_handler = PlayerIndex(cv_info_dict=cv_info_dict)
+            cv_result_index = model_handler.get_cv_index()
+            cv_result_index = json.loads(json.dumps(cv_result_index))
+            os.remove(cv_info_dict.get("temp_video_path"))  # 删除临时视频文件
+            os.remove(file_path)  # 删除临时固定帧率源视频
+            logger.info(cv_result_index)
+        else:
+            cv_info_dict["temp_video_path"] = file_path
+            model_handler = PlayerIndex(cv_info_dict=cv_info_dict)
+            logger.info(cv_info_dict)
+            cv_result_index = model_handler.get_cv_index()
+            cv_result_index = json.loads(json.dumps(cv_result_index))
+            os.remove(cv_info_dict.get("temp_video_path"))  # 删除临时视频文件
+            logger.info(cv_result_index)
+
     except Exception as err:
         logger.error(err)
         logger.error(traceback.format_exc())
@@ -87,4 +100,4 @@ def video_quality_task(video_quality_dict):
 
 
 if __name__ == '__main__':
-    print('')
+    print(VideoQualityItem.STARTAPPDOUYIN)
