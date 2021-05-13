321cebcc3d0d5e31f021ff59c36831c04b228dde
GuoJunfengMac <944194985@qq.com>
2021-05-13 14:34:23 +0800

    Merge branch 'GieGie' of git.bilibili.co:ep-platform/player-index-server into GieGie
    # Conflicts:
    #	app/third_lib/image_quality.py

--- tests/port_test.py
+++ tests/port_test.py
@@ -13,7 +13,7 @@ class TestPort(object):
                              "Chrome/51.0.2704.103 Safari/537.36"}
 
     def test_green_frame_detect(self):
-        url = "http://127.0.0.1:8091/image/quality/green-frame-detect"
+        url = "http://127.0.0.1:8090/image/quality/green-frame-detect"
         filepath = self.module_path + '/image_data/green.png'
         files = {'file': open(filepath, "rb")}
         response = requests.post(url=url, files=files, headers=self.headers)
@@ -21,10 +21,10 @@ class TestPort(object):
 
     def test_color_layer(self):
         url = "http://localhost:8090/image/quality/colorlayer-detect"
-        filepath = self.module_path + '/image_data/color_layer_detect1.png'
+        filepath = self.module_path + '/image_data/color_layer_detect2.png'
         files = [('file', ('color_layer_detect1.png', open(filepath, 'rb'), 'image/png'))]
         response = requests.request("POST", url, headers=self.headers, files=files)
-        assert response.text.find('"blue":"35.21%","green":"31.55%"') != -1
+        assert response.text.find('"blue":"36.22%","green":"15.11%"') != -1
 
     def test_horizontal_frame_detect(self):
         url = "http://localhost:8090/image/quality/horizontal-frame-detect"
