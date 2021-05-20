d9e04e5641a7138665ba5a79ed4811044b0d5934
luoyadong <luoyadong@bilibili.com>
2021-05-20 15:18:08 +0800

    心脏图像匹配算法

--- app/third_lib/image_quality.py
+++ app/third_lib/image_quality.py
@@ -12,6 +12,64 @@ from cnstd import CnStd
 from skimage.measure import compare_ssim
 
 
+class BlurredFrameDetector(object):
+    """ 花屏检测基类
+    """
+    def __init__(self, image_data):
+        self.__blurred_frame_check_server_url = "http://172.22.119.82:8601/v1/models/blurred_screen_model:predict"
+        self.image_data = image_data
+
+    @staticmethod
+    def access_model_server(img, request_url):
+        """ 访问接口
+            :param img:
+            :param request_url:
+            :return:
+        """
+        headers = {"content-type": "application/json"}
+        body = {"instances": [{"input_1": img}]}
+        try_times = 0
+        while try_times < 3:
+            try:
+                response = requests.post(request_url, data=json.dumps(body), headers=headers)
+                response.raise_for_status()
+                prediction = response.json()['predictions'][0]
+                return numpy.argmax(prediction)
+            except Exception as err:
+                print(err)
+                try_times += 1
+                continue
+        if try_times >= 3:
+            return -1
+
+    @staticmethod
+    def __laplace_image(image_np):
+        """ 花屏训练数据处理，拉普拉斯变换
+        :return:
+        """
+        img_laplace = cv2.Laplacian(image_np, cv2.CV_16S, ksize=3)
+        img_laplace = cv2.convertScaleAbs(img_laplace)
+        return img_laplace
+
+    def get_if_blurred_frame(self):
+        """ 花屏检测
+        :return:
+        """
+        img = cv2.cvtColor(self.image_data, cv2.COLOR_RGB2GRAY)
+        img_laplace = self.__laplace_image(img)
+        img = cv2.cvtColor(img_laplace, cv2.COLOR_GRAY2RGB)
+        image = Image.fromarray(img)  # 先转格式为Image 为了统一输入图像尺寸
+        predict_image = image.resize((960, 448), Image.NEAREST)
+        img = numpy.asarray(predict_image).astype(float)
+        img_list = img.tolist()
+        request_url = self.__blurred_frame_check_server_url
+        predict_result = self.access_model_server(img_list, request_url)
+        if predict_result == -1:
+            return -1
+        else:
+            return predict_result
+
+
 class ImageSplitJoint(object):
     """图像帧拼接识别类
     """
@@ -228,76 +286,86 @@ class ImageColorLayer(object):
                 self.color_dict['blue'] = red_ratio
 
 
+class ImageMatcher(object):
+    """ 通用图像匹配类， 基于sift/surf算法
+    """
+
+    def __init__(self, template_image, target_image):
+        """
+        param: template_image: 模版图像，即子图
+        param: target_image: 匹配图像，即被匹配图像
+        """
+        # 颜色空间转化
+        self.__template_image = cv2.cvtColor(template_image, cv2.COLOR_RGB2BGR)
+        self.__target_image = cv2.cvtColor(target_image, cv2.COLOR_RGB2BGR)
+        # const
+        self.__MIN_MATCH_COUNT = 10
+        self.__FLANN_INDEX_KDTREE = 1
+
+    def __calc_central_coordinates(self, coordinate_list: list):
+        """
+        """
+        pass
+
+    def get_template_image_coordinates(self):
+        """
+        """
+        sift = cv2.SIFT_create()
+        kp1, des1 = sift.detectAndCompute(self.__template_image, None)
+        kp2, des2 = sift.detectAndCompute(self.__target_image, None)
+        index_params = dict(algorithm=self.__FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1, des2, k=2)
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
+        if len(good) > self.__MIN_MATCH_COUNT:
+            src_pts = numpy.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = numpy.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            h, w, _ = self.__template_image.shape
+            pts = numpy.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            dst = cv2.perspectiveTransform(pts, M)
+            central_coordinates = numpy.int32(dst.mean(0)).tolist()
+            if len(central_coordinates) >= 1:
+                central_coordinates = central_coordinates[0]
+            else:
+                central_coordinates = []
+        else:
+            central_coordinates = []
+
+        # 验证测试代码
+        # cv2.circle(self.__target_image, tuple(central_coordinates), 10, (0, 0, 255), 4)
+        # cv2.imwrite('test.png', self.__target_image)
+
+        return {"match_coordinates": central_coordinates}
+
+
 class ImageQualityIndexGenerator(object):
     """ 图像质量指标库
     """
 
-    def __init__(self, image_file):
+    def __init__(self, image_file, target_image_file=None):
         """
+        param: image_file: 必要输入的图像
+        param: target_image_file: 多图像可选项
         """
-        self.__blurred_frame_check_server_url = "http://172.22.119.82:8601/v1/models/blurred_screen_model:predict"
         self.image_data = self.__bytesIO2img(image_file)
+        if target_image_file:
+            self.target_image_file = self.__bytesIO2img(target_image_file)
+        else:
+            self.target_image_file = None
 
     def __bytesIO2img(self, image_file):
         """
         :return:
         """
-        # in_memory_file = io.BytesIO()
-        # image_file.save(in_memory_file)
         img = numpy.frombuffer(image_file, dtype=numpy.uint8)
         img = cv2.imdecode(img, 1)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         return img
-
-    def __access_model_server(self, img, request_url):
-
-        """访问接口
-            :param img:
-            :param request_url:
-            :return:
-        """
-        headers = {"content-type": "application/json"}
-        body = {"instances": [{"input_1": img}]}
-        try_times = 0
-        while try_times < 3:
-            try:
-                response = requests.post(request_url, data=json.dumps(body), headers=headers)
-                response.raise_for_status()
-                prediction = response.json()['predictions'][0]
-                return numpy.argmax(prediction)
-            except Exception as err:
-                print(err)
-                try_times += 1
-                continue
-        if try_times >= 3:
-            return -1
-
-    def __image_ssim_sim(self):
-        """
-        :return:
-        """
-        pass
-
-    def __image_psnr_sim(self):
-        """
-        :return:
-        """
-        pass
-
-    def __image_hash_sim(self):
-        """
-        :return:
-        """
-        pass
-
-    @staticmethod
-    def __laplace_image(image_np):
-        """ 花屏训练数据处理，拉普拉斯变换
-        :return:
-        """
-        img_laplace = cv2.Laplacian(image_np, cv2.CV_16S, ksize=3)
-        img_laplace = cv2.convertScaleAbs(img_laplace)
-        return img_laplace
 
     def get_black_white_frame_score(self):
         """ 判断是否是黑屏，白屏，蓝屏等
@@ -348,20 +416,8 @@ class ImageQualityIndexGenerator(object):
         """ 花屏检测
         :return:
         """
-        img = cv2.cvtColor(self.image_data, cv2.COLOR_RGB2GRAY)
-        img_laplace = self.__laplace_image(img)
-        img = cv2.cvtColor(img_laplace, cv2.COLOR_GRAY2RGB)
-        image = Image.fromarray(img)  # 先转格式为Image 为了统一输入图像尺寸
-        predict_image = image.resize((960, 448), Image.NEAREST)
-        img = numpy.asarray(predict_image).astype(float)
-        img_list = img.tolist()
-        request_url = self.__blurred_frame_check_server_url
-        predict_result = self.__access_model_server(img_list, request_url)
-        # print(predict_result)
-        if predict_result == -1:
-            return -1
-        else:
-            return predict_result
+        blurred_detect_handler = BlurredFrameDetector(self.image_data)
+        return blurred_detect_handler.get_if_blurred_frame()
 
     def get_image_ssim(self):
         """ 图像结构相似度相似度
@@ -380,6 +436,10 @@ class ImageQualityIndexGenerator(object):
     def get_image_colorlayer(self):
         image_colorlayer_handler = ImageColorLayer(self.image_data)
         return image_colorlayer_handler.get_colorlayer_info()
+
+    def get_image_match_result(self):
+        image_matcher_handler = ImageMatcher(template_image=self.image_data, target_image=self.target_image_file)
+        return image_matcher_handler.get_template_image_coordinates()
 
 
 if __name__ == '__main__':
