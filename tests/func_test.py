f9cd0c9dae8ba46e7ee344e84bb7b767a7cdef6f
GuoJunfengMac <944194985@qq.com>
2021-06-01 11:27:46 +0800

    Merge branch 'GieGie' of git.bilibili.co:ep-platform/player-index-server into GieGie
    # Conflicts:
    #	app/third_lib/image_quality.py

--- tests/func_test.py
+++ tests/func_test.py
@@ -50,7 +50,7 @@ class TestFunc(object):
         img = cv2.imread(filename)
         img_bytes = cv2.imencode('.jpg', img)[1]
         a = ImageIndex(img_bytes)
-        assert a.blurred_frame_detection() == -1
+        assert a.blurred_frame_detection() is True
 
     def test_frame_ocr(self):
         filename = self.module_path + '/image_data/ocr.jpg'
