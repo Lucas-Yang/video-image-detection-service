029d2332c1b570f52023e478055bf0733cf10e7f
GuoJunfengMac <944194985@qq.com>
2021-07-27 17:41:16 +0800

    paddleocr初始化移出init,否则docker服务有时会卡死

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,3 @@ sentry_sdk.init(
     traces_sample_rate=1.0
 )
 """
-from paddleocr import PaddleOCR
-import os
-base_path = os.getcwd()
-cls_path = base_path + '/model/.paddleocr/2.1/cls'
-det_path = base_path + '/model/.paddleocr/2.1/det'
-rec_path = base_path + '/model/.paddleocr/2.1/rec'
-paddle_ocr = PaddleOCR(det_model_dir=det_path, rec_model_dir=rec_path, cls_model_dir=cls_path,
-                       use_gpu=False, use_angle_cls=True, lang="ch")
